9369f80cde69b099b08ab0483be463688c21ff16
diakudza <diakudza@gmail.com>
2021-03-28 13:42:22 +0300

    js для менюшек

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,13 @@
-let button = document.getElementById('menu_btn');
 let menuVertical = document.getElementById('menu__vertical');
-let menuClose = document.getElementById('menu-close');
-button.addEventListener('click', function () {
-   menuVertical.classList.toggle('display-block');
-});
-menuClose.addEventListener('click', function () {
-   menuVertical.classList.toggle('display-block');
-});
+function menuVerticalShow(){
+	menuVertical.classList.toggle('display-block');
+}
+
+let ctlMenu = document.getElementById('ctlMenu');
+function ctlMenuClose(){
+	ctlMenu.classList.toggle('display-block');
+}
+
 let count = document.getElementById('count');
 let itCount = parseInt(count.innerText);
 let productButtons = document.querySelectorAll('.productAddBtn'); 
